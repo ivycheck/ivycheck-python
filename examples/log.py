ad0868d4871bcba88f876f76d3b3c4619341f972
Dustin Lange <dustin@deekard.com>
2023-12-01 16:57:26 +0100

    add prompt execution

---
+++ examples/log.py
@@ -0,0 +1,21 @@
+import os
+from ivycheck.ivy_client import IvyClient
+
+ivy = IvyClient(api_key=os.environ["IVYCHECK_API_KEY"])
+
+# Log a prompt execution
+ivy.PromptExecution.create(
+    project_id="3b551e85-6806-49a3-98bb-b7b58e63111a",
+    messages=[
+        {
+            "content": "Hi, I'm a chatbot. How can I help you?",
+            "role": "assistant",
+        },
+        {
+            "content": "I want to cancel my subscription.",
+            "role": "user",
+        },
+    ],
+    output="Call customer service.",
+    metrics={"full_request": 0.5},
+)
